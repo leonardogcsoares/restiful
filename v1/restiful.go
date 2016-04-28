21f76b908d451a9985ba8232647b646d751d7c8e
Leonardo Soares <leonardogcsoares93@gmail.com>
2016-04-28 15:25:19 -0300

    Adapted for use with httprouter

--- v1/restiful.go
+++ v1/restiful.go
@@ -1,17 +1,21 @@
+//Package restiful is a fork that serves as facilitator for Middleware implementation
+// using httprouter
 package restiful
 
 import (
 	"net/http"
+
+	"github.com/julienschmidt/httprouter"
 )
 
-type (
-	Handler func (w http.ResponseWriter, r *http.Request) (error)
-)
+// Handler function is a wrapper for the way HttpRouter deals with httpHandlers
+type Handler func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) error
 
-func Handle(handlers ...Handler) (http.Handler) {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+// Handle calls each of the Handlers for the API endpoint in order.
+func Handle(handlers ...Handler) httprouter.Handle {
+	return httprouter.Handle(func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 		for _, handler := range handlers {
-			err := handler(w, r)
+			err := handler(w, r, ps)
 			if err != nil {
 				w.Write([]byte(err.Error()))
 				return
